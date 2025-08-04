a733493c920ae61f91743f7474d707659e520360
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-08-04 15:44:19 +0200

    betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node = binary_tree_node(parent, value);
 	if (node == NULL)
 		return (NULL);
-	
+
 	if (parent->left != NULL)
 	{
 		temp = parent->left;
