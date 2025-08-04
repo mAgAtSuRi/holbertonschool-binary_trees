028b974a07d1ed4dfdc819e41a8d33030e68cb27
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-08-04 15:24:33 +0200

    case if child already exists added

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (parent->left != NULL)
+		return(parent->left);
+
 	node = binary_tree_node(parent, value);
 
 	if (parent != NULL)
