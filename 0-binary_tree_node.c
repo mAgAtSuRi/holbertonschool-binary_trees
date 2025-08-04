a1df8d40e141746c60aef7dba89b48609e1fe08a
Tristan <tristan.latuilliere@gmail.com>
2025-08-04 10:56:50 +0200

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,7 @@
  *
  * Return: pointer to the created node
  */
-binary_tree_t * binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
