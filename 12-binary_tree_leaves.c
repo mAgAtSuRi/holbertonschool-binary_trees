961bc18f5321eca9630b690cea33525ba00c17ed
Tristan <tristan.latuilliere@gmail.com>
2025-08-05 11:37:09 +0200

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_leaves - counts the number of leaves
  * @tree: pointer to the root of the tree to check
- * 
+ *
  * Return: number of leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
