2dd73e3854c2fca99e8f22cc89b3e37a3bdcc4c6
Tristan <tristan.latuilliere@gmail.com>
2025-08-05 12:12:13 +0200

    if node null return -1

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - measures height of a node
+ * binary_tree_height - measures height of a node tree
  * @tree: pointer to the tree to measure
  *
  * Return: size of the tree
@@ -10,16 +10,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
+	if (tree == NULL)
+		return (-1);
 
-	left_height = 1 + binary_tree_height(tree->left);
-	right_height = 1 + binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (left_height >= right_height)
-		return (left_height);
+	if (left_height > right_height)
+		return (1 + left_height);
 	else
-		return (right_height);
+		return (1 + right_height);
 }
 
 /**
