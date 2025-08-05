2c5efb99b33ee48eb50c63df7f2c5f02e9fe6ede
Tristan <tristan.latuilliere@gmail.com>
2025-08-05 12:14:55 +0200

    changed in int

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,10 +5,9 @@
  *
  * Return: size of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	int left_height, right_height;
 
 	if (tree == NULL)
 		return (-1);
