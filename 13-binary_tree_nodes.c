da980f58534177d8833676172c38483b19479073
Tristan <tristan.latuilliere@gmail.com>
2025-08-05 11:45:18 +0200

    betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,7 +6,7 @@
  *
  * Return: number of nodes with at least 1 child
  */
-size_t binary_tree_nodes( const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
